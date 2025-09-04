7fb1069e2b428c2557bd94edc137c724fa369ffb
divaydua <duadivay@gmail.com>
2025-09-04 15:58:44 +0200

    Added security filter chain

--- springSecurityTest/src/main/java/com/example/duadivay/springSecurityTest/config/SecurityConfig.java
+++ springSecurityTest/src/main/java/com/example/duadivay/springSecurityTest/config/SecurityConfig.java
@@ -2,13 +2,17 @@ package com.example.duadivay.springSecurityTest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -33,20 +37,24 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(user, admin);
     }
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public", "/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/", true)  // after login, go to "/"
-                        .permitAll()
-                )
-                .logout(logout -> logout.permitAll())
-                .csrf(csrf -> csrf.disable())
-                .headers(headers -> headers.frameOptions().disable());
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        HttpSecurity logout = httpSecurity
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/public/**").permitAll()   // give any of your 'get' request endpoint
+                                .anyRequest().authenticated())  // All other requests require authentication
 
-        return http.build();
+                .csrf(csrfConfig ->  // Disable CSRF for simplicity, not recommended for production
+                        csrfConfig
+                                .disable())
+                .sessionManagement(sessionConfig ->   // Disable JSESSIONID for simplicity, not recommended for production
+                        sessionConfig
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .formLogin(Customizer.withDefaults())  // Enable form login here I use default login
+
+                .logout(Customizer.withDefaults());// Enable logout
+
+        return httpSecurity.build();   //when you add build this throws an exception
     }
 }
